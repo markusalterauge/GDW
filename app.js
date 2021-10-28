9bbb67194ca4f660017a8444879e173a27cc66cf
markusalterauge <markus.alterauge@smail.th-koeln.de>
2021-10-28 11:22:24 +0200

    Express installiert und Hello World kopiert

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Get Methode auf localhost:3000/ oder 127.0.0.1:3000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//Startet den Server und hört auf Port 3000
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:3000`) //Magic Number
+})
